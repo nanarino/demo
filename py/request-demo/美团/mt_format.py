a2c2365b272dcd2d89aa96cb7f82e476245d097d
nanarino <kogawananari@gmail.com>
2020-09-22 17:00:41 +0800

    fix

--- py/request-demo/美团/mt_format.py
+++ py/request-demo/美团/mt_format.py
@@ -1,25 +1,21 @@
 import json
 import csv
-_id = 983467099995385
-csvFile = open(str(_id)+"_促销.csv", "w")#, encoding='utf-8')
+csvFile = open("990717024543786.csv", "w", encoding='gbk', newline='')
 writer = csv.writer(csvFile)
 fileHeader = ("spuName", "spuId", "tag", "saleVolume", "originPrice", "currentPrice", "sellStatus", "skuId", "spec", "soldStatus", "realStock", "skuPromotionInfo")
 writer.writerow(fileHeader)
-filename = str(_id)+'_促销.json'
-
-with open(filename, encoding="utf-8") as f:
+table = []
+with open ('food.json',encoding='utf-8') as f:
     data = json.loads(f.read())["data"]["categoryList"][0]["spuList"]
-    table = []
     for i in data:
         table.append(tuple([i["spuName"], i["spuId"], i["tag"], i["saleVolume"], i["originPrice"], i["currentPrice"], i["sellStatus"], i["skuList"][0]["skuId"], i["skuList"][0]["spec"], i["skuList"][0]["soldStatus"], i["skuList"][0]["realStock"], i["skuList"][0]["skuPromotionInfo"]]))
-
-'''
-from pprint import pprint
-pprint(list(table), width=60, compact=True)
-'''
+with open ('menuproducts.txt',encoding='utf-8') as f:
+    for line in f:
+        for i in json.loads(line)["data"]["spuList"]:
+            table.append(tuple([i["spuName"], i["spuId"], i["tag"], i["saleVolume"], i["originPrice"], i["currentPrice"], i["sellStatus"], i["skuList"][0]["skuId"], i["skuList"][0]["spec"], i["skuList"][0]["soldStatus"], i["skuList"][0]["realStock"], i["skuList"][0]["skuPromotionInfo"]]))
 for i in table:
     try:
         writer.writerow(i)
-    except UnicodeEncodeError:
-        pass
+    except UnicodeEncodeError as e:
+        print(e)
 csvFile.close()
