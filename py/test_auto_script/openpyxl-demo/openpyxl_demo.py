84a5f0a80ac7f4bdd0ba70b23ad19f6dda65a5f5
古川ななり <kogawananari@gmail.com>
2021-03-23 16:58:46 +0800

    DOC 修复代码格式化的副作用

--- py/test_auto_script/openpyxl-demo/openpyxl_demo.py
+++ py/test_auto_script/openpyxl-demo/openpyxl_demo.py
@@ -1,16 +1,18 @@
 from pprint import pprint
-''' @desc 只读电子表格函数
-    @param path: xlsx的文件路径
-    @param sheet_index: table的序号(0即Sheet1) 
-        自定义的table命名可能无法用下标找到导致抛错
-    @param is_read_head: 是否读取表头(即第一行)
-    @return -> 返回列表嵌套元组的结构(和数据库查询的格式结果一致)'''
+
+
 from openpyxl import load_workbook
 
 
 def read_excel_xlsx(path: str,
                     sheet_index: int = 0,
                     is_read_head: bool = False):
+    ''' @desc 只读电子表格函数
+    @param path: xlsx的文件路径
+    @param sheet_index: table的序号(0即Sheet1) 
+        自定义的table命名可能无法用下标找到导致抛错
+    @param is_read_head: 是否读取表头(即第一行)
+    @return -> 返回列表嵌套元组的结构(和数据库查询的格式结果一致)'''
     wb = load_workbook(path, read_only=True)
     table = wb[wb.sheetnames[sheet_index]]
     #最大行
@@ -47,13 +49,14 @@ head = ['机构名称', '优惠卡编号', '姓名', '性别', '身份证号', '
 #表头插入到数据中
 querylist.insert(0, head)
 
-''' @desc 新写入电子表格函数
-    @param path: 要生成xlsx文件的路径
-    @param value: 要写入的数据'''
+
 from openpyxl import Workbook
 
 
 def write_excel_xlsx(path: str, sheet_index: int, value: list):
+    ''' @desc 新写入电子表格函数
+    @param path: 要生成xlsx文件的路径
+    @param value: 要写入的数据'''
     index = len(value)
     wb = Workbook()
     #sheet = wb.active 等价于下一行传0,即"Sheet1" 也可以用sheet.title自定义
