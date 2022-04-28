edaedd7f14b9ecea2297d92eb2b9f60156ef3f8b
☘ <kogawananari@gmail.com>
2022-04-28 19:04:26 +0800

    del   删除一些有的没的

--- js/genera/bind.js
+++
@@ -1,8 +0,0 @@
-Function.prototype.bind = function () {
-    var bindThis = arguments[0]
-    var arg = Array.prototype.slice.call(arguments, 1)
-    var that = this
-    return function () {
-        that.apply(bindThis, arg)
-    }
-}
