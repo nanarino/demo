9f2c7be3be71e60e4f1d7d1bade0ca035c857fa5
☘ <kogawananari@gmail.com>
2022-07-01 14:49:41 +0800

    update tab size

--- js/css-paint-api-cases/smoothCorner.js
+++ js/css-paint-api-cases/smoothCorner.js
@@ -1,32 +1,32 @@
 //https://drafts.css-houdini.org/css-paint-api/
 class SmoothCorner {
 
-    static get inputProperties() { return ['--smooth-level']; }
+  static get inputProperties() { return ['--smooth-level']; }
 
-    paint(ctx, size, properties) {
-        ctx.fillStyle = 'black'
-        const level = properties.get('--smooth-level').toString()
-        const n = level
-        let m = n
-        if (n > 100) m = 100
-        if (n < 0.00000000001) m = 0.00000000001
-        const r = size.width / 2
-        const w = size.width / 2
-        const h = size.height / 2
-        ctx.beginPath();
-        for (let i = 0; i < (2 * r + 1); i++) {
-            const x = (i - r) + w
-            const y = (Math.pow(Math.abs(Math.pow(r, m) - Math.pow(Math.abs(i - r), m)), 1 / m)) + h
-            i ? ctx.lineTo(x, y) : ctx.moveTo(x, y)
-        }
-        for (let i = (2 * r); i < (4 * r + 1); i++) {
-            const x = (3 * r - i) + w
-            const y = (-Math.pow(Math.abs(Math.pow(r, m) - Math.pow(Math.abs(3 * r - i), m)), 1 / m)) + h
-            ctx.lineTo(x, y)
-        }
-        ctx.closePath()
-        ctx.fill()
+  paint(ctx, size, properties) {
+    ctx.fillStyle = 'black'
+    const level = properties.get('--smooth-level').toString()
+    const n = level
+    let m = n
+    if (n > 100) m = 100
+    if (n < 0.00000000001) m = 0.00000000001
+    const r = size.width / 2
+    const w = size.width / 2
+    const h = size.height / 2
+    ctx.beginPath();
+    for (let i = 0; i < (2 * r + 1); i++) {
+      const x = (i - r) + w
+      const y = (Math.pow(Math.abs(Math.pow(r, m) - Math.pow(Math.abs(i - r), m)), 1 / m)) + h
+      i ? ctx.lineTo(x, y) : ctx.moveTo(x, y)
     }
+    for (let i = (2 * r); i < (4 * r + 1); i++) {
+      const x = (3 * r - i) + w
+      const y = (-Math.pow(Math.abs(Math.pow(r, m) - Math.pow(Math.abs(3 * r - i), m)), 1 / m)) + h
+      ctx.lineTo(x, y)
+    }
+    ctx.closePath()
+    ctx.fill()
+  }
 }
 
 registerPaint('smooth-corners', SmoothCorner);
--- js/运动框架/运动框架.js
+++ js/运动框架/运动框架.js
@@ -1,5 +1,5 @@
 function getStyle(ele) {
-    return ele.currentStyle || getComputedStyle(ele)
+  return ele.currentStyle || getComputedStyle(ele)
 }
 
 //简单的解决兼容问题
@@ -7,33 +7,33 @@ window.requestAnimationFrame = window.requestAnimationFrame || function (cb) { r
 window.cancelAnimationFrame = window.cancelAnimationFrame || clearTimeout
 
 function animation(ele, data = {}, time = 500, cb) {
-    /*
-     * ele => 已获取的HTML标签对象
-     * data => 需要改变的样式:目标数值 可以带单位
-     * time => 改变样式所需要的总时间 单位是毫秒
-     * 例如 : animation(box,{width:'1000px',height:'1000px'},5000)
-     */
-    var startValue = {}
-    var changeValue = {}
-    var startTime = new Date()
-    var eleStart = getStyle(ele)
-    for (var key in data) {
-        startValue[key] = isNaN(Number.parseFloat(eleStart[key])) ? 0 : Number.parseFloat(eleStart[key])
-        changeValue[key] = Number.parseFloat(data[key]) - startValue[key]
+  /*
+   * ele => 已获取的HTML标签对象
+   * data => 需要改变的样式:目标数值 可以带单位
+   * time => 改变样式所需要的总时间 单位是毫秒
+   * 例如 : animation(box,{width:'1000px',height:'1000px'},5000)
+   */
+  var startValue = {}
+  var changeValue = {}
+  var startTime = new Date()
+  var eleStart = getStyle(ele)
+  for (var key in data) {
+    startValue[key] = isNaN(Number.parseFloat(eleStart[key])) ? 0 : Number.parseFloat(eleStart[key])
+    changeValue[key] = Number.parseFloat(data[key]) - startValue[key]
+  }
+  run()
+  function run() {
+    var nowTime = new Date() - startTime
+    var t1 = nowTime / time
+    for (var key in changeValue) {
+      var val = t1 * changeValue[key] + startValue[key]
+      ele.style[key] = val + 'px'
     }
-    run()
-    function run() {
-        var nowTime = new Date() - startTime
-        var t1 = nowTime / time
-        for (var key in changeValue) {
-            var val = t1 * changeValue[key] + startValue[key]
-            ele.style[key] = val + 'px'
-        }
-        if (t1 >= 1) {
-            cancelAnimationFrame(run)
-            cb && cb()
-        } else {
-            requestAnimationFrame(run)
-        }
+    if (t1 >= 1) {
+      cancelAnimationFrame(run)
+      cb && cb()
+    } else {
+      requestAnimationFrame(run)
     }
+  }
 }
