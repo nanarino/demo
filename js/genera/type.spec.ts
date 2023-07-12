de3dd317c46012a673beb3f0a03f26ad9b9c3da6
☘ <kogawananari@gmail.com>
2023-07-12 20:08:47 +0800

    :notes:

---
+++ js/genera/type.spec.ts
@@ -0,0 +1,15 @@
+import type from './type.mjs';
+
+console.log(type(1));
+console.log(type(true));
+console.log(type('string'));
+console.log(type(undefined));
+console.log(type(null));
+console.log(type({}));
+console.log(type([]));
+console.log(type(new Set()));
+console.log(type(new Map()));
+console.log(type(new Date()));
+console.log(type(/(.*)/));
+console.log(type(()=>{}));
+console.log(type(async function*(){}));
