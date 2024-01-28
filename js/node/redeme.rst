623e0d89d4c7f076cee8bb388b7ffffc47a89291
☘ <kogawananari@gmail.com>
2024-01-28 14:20:55 +0800

    :truck:

---
+++ js/node/redeme.rst
@@ -0,0 +1,3 @@
+* node: nodejs脚本
+    - ``cd node; npx esno **/src/*.ts``
+    - 通过以上命令或直接使用vscode的run-code插件运行同名的 ``*.spec.*`` 来测试运行
--- js/practice/jquery-cases/redirect.js
+++
@@ -1,9 +0,0 @@
-function countDown(secs, surl) {
-    var $jumpTo = $(".rewards-popover h4 span");
-    $jumpTo.html(secs)
-    if (--secs > 0) {
-        setTimeout("countDown(" + secs + ",'" + surl + "')", 1000);
-    } else {
-        location.href = surl;
-    }
-}//countDown(3, '/index');
